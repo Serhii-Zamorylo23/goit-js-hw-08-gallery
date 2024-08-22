7f4269aaf4b3e4a9b8b0ceea5dcffea57e834830
Serhii-Zamorylo23 <zamorylo@i.ua>
2024-08-22 19:07:24 +0300

    ор

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const gallery=document.querySelector(".gallery")
-const lightbox=document.querySelector(".lightbox")
+const gallery = document.querySelector(".js-gallery")
+const lightbox = document.querySelector(".lightbox")
 const galleryItems = [
   {
     preview:
@@ -10,21 +10,21 @@ const galleryItems = [
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Container Haulage Freight',
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      ' https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpghttps://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
     description: 'Flower Blooms',
@@ -65,13 +65,14 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
-galleryItems.forEach(({preview,original,description})=>{
-  gallery.innerHTML+=`
-    <li class="gallery__item">
+
+const renderGalleryItems = galleryItems.map(({ preview, original, description,...galleryItems}) => {
+  gallery.innerHTML += `
+  <li class="gallery__item">
   <a
     class="gallery__link"
     href=${original}
-  >
+
     <img
       class="gallery__image"
       src=${preview}
@@ -81,4 +82,8 @@ galleryItems.forEach(({preview,original,description})=>{
   </a>
 </li>
   `;
+})
+
+lightbox.addEventListener("click", () => {
+
 })
